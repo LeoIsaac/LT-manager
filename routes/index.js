5ffb3dcfd8afb1e8ae0b156453b9649886b4597e
Reo Sato <isaac@nicofun.club>
2016-10-03 14:23:54 +0900

    Add redis

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if(notEmpty(req.session.passport)) {
-    res.render('index', { title: "Welcome to " + req.session.passport.user.displayName });
+  var passport = req.session.passport;
+  if(isLogin(passport)) {
+    res.render('index', { title: passport.user.displayName });
   } else {
     res.render('error', {
       message: 'Please login',
@@ -13,7 +14,7 @@ router.get('/', function(req, res, next) {
   }
 });
 
-function notEmpty(v) {
+function isLogin(v) {
   return Boolean(
     typeof v == 'string' ? v && v != 0 : typeof v == 'object' ? v && Object.keys(v).length : v
   );
--- routes/oauth.js
+++ routes/oauth.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var config = require('config');
 var passport = require('passport');
+var redis = require('redis');
+var client = redis.createClient(config.redis_port, config.redis_host);
 
 /* GET home page. */
 router.get('/login', passport.authenticate("twitter"));
 
 router.get('/callback', passport.authenticate("twitter", {
-  successRedirect: "/",
   failureRedirect: "/"
-}));
+}), function(req, res) {
+  var user = req.user;
+  client.hget(user.id, "name", function(err, val) {
+    if(val == null) {
+      client.hmset(user.id,
+        "name", user.displayName,
+        "img",  user.photos[0].value
+      );
+    } else {
+      console.log("既に登録されています。");
+    }
+  });
+  res.redirect("/");
+});
 
 router.get('/logout', function(req, res) {
   req.logout();
