4cfbca12bdf4151b17264f3489729654ec34cf25
Reo Sato <isaac@nicofun.club>
2016-10-05 19:45:36 +0900

    Modify setting page

--- routes/setting.js
+++ routes/setting.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('This is setting page!');
+  var passport = req.session.passport;
+  if(!isLogin(passport)) res.redirect("/");
+  res.render('setting', {
+    name: passport.user.displayName,
+    img: passport.user.photos[0].value,
+    isLogin: true
+  });
 });
+
+function isLogin(v) {
+  return Boolean(
+    typeof v == 'string' ? v && v != 0 : typeof v == 'object' ? v && Object.keys(v).length : v
+  );
+}
 
 module.exports = router;
